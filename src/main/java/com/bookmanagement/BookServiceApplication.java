f5f83e9e1c3d5a05ae9e6d08786e0d675a0ab9f0
youssef fakih <y_fakih@etu.enset-media.ac.ma>
2025-01-26 15:08:27 +0100

    tp v

--- src/main/java/com/bookmanagement/BookServiceApplication.java
+++ src/main/java/com/bookmanagement/BookServiceApplication.java
@@ -1,8 +1,10 @@
 package com.bookmanagement;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 
 import javax.sql.DataSource;
@@ -12,7 +14,12 @@ public class BookServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookServiceApplication.class, args);
-		System.out.println("--------------" + new DataSourceProperties().getUrl());
+	}
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> {
+			System.out.println("--------------" );
+		};
 	}
 
 }
