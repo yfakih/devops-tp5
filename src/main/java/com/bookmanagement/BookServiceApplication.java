78ead0851bf4274418474713800b94c46d343607
youssef fakih <y_fakih@etu.enset-media.ac.ma>
2025-01-26 14:40:24 +0100

    tp v7

--- src/main/java/com/bookmanagement/BookServiceApplication.java
+++ src/main/java/com/bookmanagement/BookServiceApplication.java
@@ -12,7 +12,7 @@ public class BookServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookServiceApplication.class, args);
-		System.out.println(new DataSourceProperties().getUrl());
+		System.out.println("--------------" + new DataSourceProperties().getUrl());
 	}
 
 }
